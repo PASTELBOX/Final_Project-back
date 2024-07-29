a2f166a4c47cdf9583574e1ede71933c699accc7
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-07-29 19:03:01 +0900

    back
    back

---
+++ src/test/java/com/example/final_projectback/FinalProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_projectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
